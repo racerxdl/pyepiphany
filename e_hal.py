8c7b5591bb08fee6374ae6d4e1530f27f988a931
Lucas Teske <lucas@teske.com.br>
2014-10-07 17:08:58 -0300

    Added few place holders

--- e_hal.py
+++ e_hal.py
@@ -48,28 +48,55 @@ def e_write(dev, row, col, to_addr, buf, size):
 	print "TODO: Not implemented!"
 	pass
 
-'''
 
-///////////////////////////
-// System control functions
-#define e_reset e_reset_system
-int     e_reset_system();
-int     e_reset_chip();
-int     e_reset_group(e_epiphany_t *dev);
-int     e_start(e_epiphany_t *dev, unsigned row, unsigned col);
-int     e_start_group(e_epiphany_t *dev);
-int     e_signal(e_epiphany_t *dev, unsigned row, unsigned col);
-int     e_halt(e_epiphany_t *dev, unsigned row, unsigned col);
-int     e_resume(e_epiphany_t *dev, unsigned row, unsigned col);
+###########################
+## System control functions
+
+def     e_reset_system():
+	print "TODO: Not implemented!"
+	pass
+
+e_reset = e_reset_system
+
+def     e_reset_chip():
+	print "TODO: Not implemented!"
+	pass
+def     e_reset_group(dev):
+	print "TODO: Not implemented!"
+	pass
+def     e_start(dev, row, col):
+	print "TODO: Not implemented!"
+	pass
+def     e_start_group(dev):
+	print "TODO: Not implemented!"
+	pass
+def     e_signal(dev, row, col):
+	print "TODO: Not implemented!"
+	pass
+def     e_halt(dev, row, col):
+	print "TODO: Not implemented!"
+	pass
+def     e_resume(dev, row, col):
+	print "TODO: Not implemented!"
+	pass
 
 
-////////////////////
-// Utility functions
-unsigned e_get_num_from_coords(e_epiphany_t *dev, unsigned row, unsigned col);
-void     e_get_coords_from_num(e_epiphany_t *dev, unsigned corenum, unsigned *row, unsigned *col);
-//
-e_bool_t e_is_addr_on_chip(void *addr);
-e_bool_t e_is_addr_on_group(e_epiphany_t *dev, void *addr);
-//
-e_hal_diag_t e_set_host_verbosity(e_hal_diag_t verbose);
-'''
+####################
+## Utility functions
+def e_get_num_from_coords(dev, row, col):
+	print "TODO: Not implemented!"
+	pass
+def e_get_coords_from_num(dev, corenum, row, col):
+	print "TODO: Not implemented!"
+	pass
+##
+def e_is_addr_on_chip(addr):
+	print "TODO: Not implemented!"
+	pass
+def e_is_addr_on_group(dev, addr):
+	print "TODO: Not implemented!"
+	pass
+##
+def e_set_host_verbosity(verbose):
+	print "TODO: Not implemented!"
+	pass
